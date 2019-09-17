632a87d1af9e065a260906b3ce2ac6ee4dc67c2e
Abhishek Thakur <abhishekthakurbir@gmail.com>
2019-09-18 01:05:46 +0530

    Added apollo-graphql changes for collection, header & cartIcon

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyA0mb71slXadE9YFiPS9sDRZPr32vC3_YY",
+  authDomain: "thakur-db.firebaseapp.com",
+  databaseURL: "https://thakur-db.firebaseio.com",
+  projectId: "thakur-db",
+  storageBucket: "",
+  messagingSenderId: "401390780655",
+  appId: "1:401390780655:web:4778f41c052b5a34"
 };
 
 firebase.initializeApp(config);
